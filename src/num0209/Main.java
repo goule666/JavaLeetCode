20bb02966e2e31b89c2268ea7270fd124b68dec0
niewenlong <niewenlong@corp.netease.com>
2022-07-04 14:57:01 +0800

    [feature]209. 长度最小的子数组 https://leetcode.cn/problems/minimum-size-subarray-sum/ 滑动窗口写法 个人感觉类似双指针

--- src/num0209/Main.java
+++ src/num0209/Main.java
@@ -8,20 +8,18 @@ package num0209;
  */
 public class Main {
     public int minSubArrayLen(int target, int[] nums) {
+        int sum = 0;
+        int left = 0;
         int result = 0;
-        int sum;
-        for (int i = 0; i < nums.length; i++) {
-            sum = 0;
-            for (int j = i; j < nums.length; j++) {
-                sum += nums[j];
-                if (sum >= target) {
-                    if (result != 0) {
-                        result = Math.min((j - i + 1), result);
-                    } else {
-                        result = j - i + 1;
-                    }
-                    break;
+        for (int right = 0; right < nums.length; right++) {
+            sum = sum + nums[right];
+            while (sum >= target) {
+                if (result == 0) {
+                    result = right - left + 1;
+                } else {
+                    result = Math.min(result, right - left + 1);
                 }
+                sum = sum - nums[left++];
             }
         }
         return result;
@@ -30,6 +28,6 @@ public class Main {
 
     public static void main(String[] args) {
         Main main = new Main();
-        main.minSubArrayLen(7, new int[]{});
+        System.out.println(main.minSubArrayLen(7, new int[]{2, 3, 1, 2, 4, 3}));
     }
 }
