9cc3d8c1f9f28dd000ab977d4c41e05c99cffd1b
niewenlong <niewenlong@corp.netease.com>
2022-08-25 13:02:16 +0800

    [feature]19. 删除链表的倒数第 N 个结点 https://leetcode.cn/problems/remove-nth-node-from-end-of-list/

---
+++ src/num0019/Solution.java
@@ -0,0 +1,52 @@
+package num0019;
+
+/**
+ * @author niewenlong
+ * @date 2022/8/18 12:29
+ * @description 19. 删除链表的倒数第 N 个结点
+ * @leetCodeUrl <a href="https://leetcode.cn/problems/remove-nth-node-from-end-of-list/">...</a>
+ **/
+public class Solution {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) {
+            return null;
+        }
+        ListNode dummy = new ListNode(-1, head);
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        while (n-- > 0) {
+            fast = fast.next;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        if (slow.next != null) {
+            slow.next = slow.next.next;
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.removeNthFromEnd(new ListNode(1, new ListNode(2)), 1);
+
+    }
+}
