0b768d426964498b433c3fd56101d65748b61f99
niewenlong <niewenlong@corp.netease.com>
2022-11-28 20:01:18 +0800

    [feature]344. 反转字符串 https://leetcode.cn/problems/reverse-string/

---
+++ src/com/goule666/leetcode/string/num0344/Solution.java
@@ -0,0 +1,27 @@
+package com.goule666.leetcode.string.num0344;
+
+/**
+ * @author niewenlong
+ * @date 2022/11/28 19:56
+ * @description 344. 反转字符串
+ * @leetCodeUrl <a href="https://leetcode.cn/problems/reverse-string/">...</a>
+ **/
+public class Solution {
+
+    public void reverseString(char[] s) {
+        if (s == null || s.length == 0) {
+            return;
+        }
+        char temp;
+        for (int i = 0; i < s.length / 2; i++) {
+            temp = s[s.length - i - 1];
+            s[s.length - i - 1] = s[i];
+            s[i] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.reverseString(new char[]{'h', 'e', 'l', 'l', 'o'});
+    }
+}
